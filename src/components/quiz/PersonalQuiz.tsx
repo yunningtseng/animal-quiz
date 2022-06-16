af27b56c5e5474092115c21ba6920bfd4db91c54
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-06-16 20:19:42 +0800

    - feat: create personal quiz

--- src/components/quiz/PersonalQuiz.tsx
+++ src/components/quiz/PersonalQuiz.tsx
@@ -1,89 +1,134 @@
-import { query, collection, getDocs } from 'firebase/firestore';
-// import { query, collection, getDocs, doc, setDoc } from 'firebase/firestore';
-import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  query, collection, onSnapshot,
+} from 'firebase/firestore';
+import { useState, useEffect } from 'react';
 import { db } from '../../utils/firebaseInit';
 
-function PersonalQuiz() {
-  useEffect(() => {
-    const q = query(collection(db, 'user'));
+interface Options {
+  pic:string;
+  name: string;
+}
+interface Question {
+  type: string;
+  mainPic: string;
+  options: Options[];
+  title: string;
+  answer: number | number[];
+  inputType:string;
+}
 
-    // getDocs(q).then((snapshot) => {
-    //   snapshot.forEach((_doc) => {
-    //     // console.log(doc.id, ' => ', doc.data());
-    //     // setDoc(doc(db, 'users', _doc.id), _doc.data());
-    //   });
-    // });
+function PersonalQuiz() {
+  const [questionList, setQuestionList] = useState<Question[]>([]);
+  // 題目
+  const [questionIndex, setQuestionIndex] = useState<number>(0);
+  // 呈現答案
+  const [checkAnswer, setCheckAnswer] = useState<boolean>(true);
+  // 分數
+  const [score, setScore] = useState<number>(0);
+  // 題目長度
+  const questionLength = questionList.length;
+
+  useEffect(() => {
+    const q = query(collection(db, 'questions'));
+
+    onSnapshot(q, (snapshot) => {
+      const list:Question[] = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data() as Question;
+        list.push(data);
+      });
+      setQuestionList(list);
+    });
   }, []);
+  // console.log(questionList);
+
+  function options() {
+    return questionList[questionIndex]?.options.map((option, index) => (
+      // ? key 要改
+      <div className="flex items-center">
+        <span />
+        {index + 1}
+        <input type={questionList[questionIndex].inputType} name={questionList[questionIndex].inputType} />
+        {option.name}
+        {questionList[questionIndex]?.options[index].pic === '' ? <div /> : <img src={questionList[questionIndex]?.options[index].pic} alt="img" className="w-40" /> }
+        <div />
+      </div>
+    ));
+  }
 
   return (
     <div>
       <div>Animal Quiz</div>
-      <div className="flex">
-        <div>得分:</div>
+      <div className="flex justify-between">
+        <div>
+          得分:
+          {score}
+        </div>
         <div>時間:</div>
       </div>
-      {/* 複選 */}
-      <div>
-        <p>答對囉</p>
-        <p>Q: 請問下列哪些是台灣黑熊的特徵?</p>
-        <div className="flex items-center">
-          <input type="checkbox" className="" />
-          <div>熊</div>
-        </div>
-        <div className="flex items-center">
-          <input type="checkbox" />
-          <div>熊</div>
-        </div>
-        <div className="flex items-center">
-          <input type="checkbox" />
-          <div>熊</div>
-        </div>
-        <div className="flex items-center">
-          <input type="checkbox" />
-          <div>熊</div>
-        </div>
-        <button type="button">確認</button>
-        <button type="button">下一題</button>
-      </div>
 
-      {/* 單選 */}
+      {questionIndex !== questionLength && (
       <div>
-        <p>答對囉</p>
-        <p>Q: 請問下列哪個是台灣黑熊的特徵?</p>
-        <div className="flex items-center">
-          <input type="radio" className="" />
-          <div>熊</div>
-        </div>
-        <div className="flex items-center">
-          <input type="radio" />
-          <div>熊</div>
-        </div>
-        <div className="flex items-center">
-          <input type="radio" />
-          <div>熊</div>
-        </div>
-        <div className="flex items-center">
-          <input type="radio" />
-          <div>熊</div>
-        </div>
-        <button type="button">確認</button>
-        <button type="button">下一題</button>
-      </div>
+        {!checkAnswer && (
+        <p>答對囉</p>)}
 
-      {/* 是非 */}
-      <div>
-        <p>Q: 請問下列特徵是否正確?</p>
-        <div className="flex items-center">
-          <input type="radio" className="" />
-          <div>O</div>
+        <div>
+          {/* - 題目 */}
+          <div className="mb-4">
+            <p>
+              <span />
+              {'Q1: '}
+              {questionList[questionIndex]?.title}
+            </p>
+
+            <div>
+              {questionList[questionIndex]?.mainPic === '' ? <div /> : <img src={questionList[questionIndex]?.mainPic} alt="img" className="w-48" /> }
+            </div>
+          </div>
+
+          {/* - 選項 */}
+          <div className="flex flex-wrap">
+            {options()}
+          </div>
+
+          {checkAnswer && (
+          <button
+            type="button"
+            onClick={() => {
+              setCheckAnswer(false);
+              setScore((prev) => (prev + 30));
+            }}
+          >
+            確認
+          </button>
+          )}
+
+          {!checkAnswer && (
+          <button
+            type="button"
+            onClick={() => {
+              setQuestionIndex((prev) => (prev + 1));
+              setCheckAnswer(true);
+            }}
+          >
+            下一題
+          </button>
+          )}
+
         </div>
-        <div className="flex items-center">
-          <input type="radio" />
-          <div>X</div>
-        </div>
-        <button type="button">確認</button>
-        <button type="button">下一題</button>
       </div>
+      )}
+
+      <Link to="/quiz-result">
+        {questionLength === questionIndex && (
+        <button
+          type="button"
+        >
+          作答結束
+        </button>
+        )}
+      </Link>
     </div>
   );
 }
--- src/components/quizResult/ResultRecorder.tsx
+++ src/components/quizResult/ResultRecorder.tsx
@@ -1,11 +1,72 @@
+import { Link } from 'react-router-dom';
+import {
+  query, collection, onSnapshot, where,
+} from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { db } from '../../utils/firebaseInit';
+
+interface Data {
+  answer: number | number[];
+  correct: boolean;
+  questionId:string;
+}
+
+interface Response {
+  id: string;
+  score: number;
+  // startTime:Timestamp;
+  totalTime: string;
+  answer: number;
+  userName:string;
+  data:Data[]
+}
+
 function ResultRecorder() {
+  // - 篩出特定玩家的回答
+  const [responseList, setResponseList] = useState<Response[]>([]);
+
+  useEffect(() => {
+    const q = query(collection(db, 'responses'), where('userName', '==', 'yunning'));
+
+    onSnapshot(q, (snapshot) => {
+      const list:Response[] = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data() as Response;
+        list.push(data);
+      });
+      setResponseList(list);
+    });
+  }, []);
+
+  // console.log(responseList);
+
+  // - 篩出某次測驗作答的 questionId
+  // const questionsId:[] = [];
+  // // ? 要怎麼去篩想要呈現的那一筆的 response
+  // responseList[0]?.data.forEach((qId) => {
+  //   questionsId.push(qId.questionId);
+  // });
+
+  // console.log(questionsId);
+
+  // - 根據 questionsId，去 query firebase questions 的題目
+
   return (
     <div>
       <div>測驗結果</div>
       <div className="flex">
-        <div>得分:</div>
-        <div>時間:</div>
+        {/* ? 目前先寫死 */}
+        <div>
+          得分:
+          {responseList[0]?.score}
+        </div>
+        <div>
+          時間:
+          {responseList[0]?.totalTime}
+
+        </div>
       </div>
+
       <div>
         <div className="flex">
           <p>(O)</p>
@@ -14,16 +75,11 @@ function ResultRecorder() {
             <p>A: XXXXXXX</p>
           </div>
         </div>
-
-        <div className="flex">
-          <p>(O)</p>
-          <div>
-            <p>Q: XXXXXXXXXXXX</p>
-            <p>A: XXXXXXX</p>
-          </div>
-        </div>
       </div>
-      <button type="button">返回遊戲選單</button>
+
+      <Link to="/quiz">
+        <button type="button">返回遊戲選單</button>
+      </Link>
     </div>
   );
 }
