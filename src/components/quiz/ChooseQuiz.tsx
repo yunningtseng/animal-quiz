0dd5a670e6796dfe1b2d8be3a998eacabf6adea7
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-07-21 01:02:08 +0800

    - refactor: remove redundant code - fix: tailwind problem

--- src/components/quiz/ChooseQuiz.tsx
+++ src/components/quiz/ChooseQuiz.tsx
@@ -1,12 +1,10 @@
 import ChooseQuizBox from './ChooseQuizBox';
 
-const quizTypeList = ['一般模式', '限時挑戰', '多人競賽'];
-
 function ChooseQuiz() {
   return (
     <div className="w-[15rem] sm:w-[25rem] md:w-[40rem] flex flex-wrap justify-center sm:justify-between items-center mt-10 mx-auto">
-      {quizTypeList.map((quizType) => (
-        <ChooseQuizBox key={quizType} quizType={quizType} />
+      {[0, 1, 2].map((index) => (
+        <ChooseQuizBox key={index} index={index} />
       ))}
     </div>
   );
--- src/components/quiz/ChooseQuizBox.tsx
+++ src/components/quiz/ChooseQuizBox.tsx
@@ -5,44 +5,31 @@ import tiger from '../../images/tiger.png';
 import penguin from '../../images/penguin.png';
 
 interface ChooseQuizProps {
-  quizType: string;
+  index: number;
 }
 
-const quiz: { [key: string]: string } = {
-  一般模式: 'normal',
-  限時挑戰: 'time-challenge',
-  多人競賽: 'room-menu',
-};
+const quizTypeList = ['一般模式', '限時挑戰', '多人競賽'];
+const linkList = ['normal', 'time-challenge', 'room-menu'];
+const picList = [turtle, tiger, penguin];
+const picSizeList = ['w-36', 'w-24', 'w-22'];
 
-const pic: { [key: string]: string } = {
-  一般模式: turtle,
-  限時挑戰: tiger,
-  多人競賽: penguin,
-};
-
-const picSize: { [key: string]: string } = {
-  一般模式: '9rem',
-  限時挑戰: '6rem',
-  多人競賽: '5.5rem',
-};
-
-function ChooseQuiz({ quizType }: ChooseQuizProps) {
+function ChooseQuiz({ index }: ChooseQuizProps) {
   return (
-    <Link to={`/quiz/${quiz[quizType]}`}>
+    <Link to={`/quiz/${linkList[index]}`}>
       <motion.div
         className="flex-col border rounded-xl w-44 h-48 cursor-pointer mt-5 sm:mt-0"
         aria-hidden="true"
         whileHover={{ scale: 1.1 }}
       >
         <p className="py-1 rounded-t-xl bg-dark text-white text-center text-xl font-bold">
-          {quizType}
+          {quizTypeList[index]}
         </p>
         <img
-          src={pic[quizType]}
+          src={picList[index]}
           alt="img"
-          className={`w-[${picSize[quizType]}] ${
-            pic[quizType] === 'turtle' ? '' : 'mt-3'
-          } ${quizType === '一般模式' ? 'pt-10' : ''} object-cover mx-auto`}
+          className={`${picSizeList[index]} ${
+            index === 0 ? 'pt-10' : 'mt-3'
+          } object-cover mx-auto`}
         />
       </motion.div>
     </Link>
